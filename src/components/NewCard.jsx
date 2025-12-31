0475ccd6e4962648efc8fbb342c80a944d27c8be
artem <artemgar2006@gmail.com>
2025-12-31 16:15:09 +0100

    Import words/new removing system

--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -1,9 +1,116 @@
 import "../styles/new_card.css";
 import { useState } from "react";
+import green_icon from "../assets/icons/green_delete_icon.svg";
+import blue_icon from "../assets/icons/blue_delete_icon.svg";
 
 export default function NewCard() {
     const [quantity, setQuantity] = useState(0);
     const [wordFields, setWordFields] = useState([]);
+    const [importTransWords, setImportTransWords] = useState([]);
+     const [importDefWords, setImportDefWords] = useState([]);
+
+    const importedFields = importedWords();
+    
+    function importedWords() {
+        let tempOutput = [];
+        for(var i = 0; i < importDefWords.length; i++) {
+        tempOutput.push(
+            <div className="card-form-word-container-main" key={i} data-idimport={i}>
+                <div className="card-form-word-container import">
+                    <div className="card-word-default-container import">
+                        <label htmlFor="cardWordDefault" className="card-word-label">Enter a word: </label>
+                        <input type="text" id="cardWordDefault" name="cardWordDefault" className="card-word-input" placeholder="Enter a word" value={importDefWords[i]} readOnly disabled/>
+                    </div>
+                    
+                    <div className="card-word-separator-container import">
+                        <hr className="navigation-button-separator import"/>
+                    </div>
+
+                    <div className="card-word-translation-container import">
+                        <label htmlFor="cardWordTranslation" className="card-word-label">Enter a translation: </label>
+                        <input type="text" id="cardWordTranslation" name="cardWordTranslation" className="card-word-input" placeholder="Enter a translation" value={importTransWords[i]} readOnly disabled/>
+                    </div>
+                </div>
+                <div className="card-word-button-remove-container import">
+                    <button className="card-word-button-remove import" type="button" onClick={(e) => handleRemoveClick(e)}>
+                        <img src={blue_icon} alt="Delete Icon" className="button-delete-image import"/>
+                    </button>
+                </div>
+            </div>
+        );
+        }
+        return tempOutput;
+    }
+
+    function handleImportClick() {
+            alert("You should divide your import text in a correct way. Example: Word default%word translated;");
+            alert("Please, put your text into the next window!");
+            let data = prompt();
+            if(data === null || data === "" || data === undefined) {
+                alert("Import can not be empty!");
+                return;
+            }
+            let defaultWords = [];
+            let translatedWords = [];
+            const splittedArray = [];
+            let temporaryWord = "";
+            for(var i = 0; i < data.length; i++) {
+                if(data[i] !== ";") {
+                    temporaryWord += data[i];
+                    continue;
+                } 
+                splittedArray.push(temporaryWord);
+                temporaryWord = "";
+            }
+            splittedArray.map(item => {
+                let res = item.split("%");
+                defaultWords.push(res[0]);
+                translatedWords.push(res[1]);
+                return
+            })
+
+            if(defaultWords.length !== translatedWords.length || defaultWords.length === 0 || translatedWords.length === 0) {
+                alert("A word is missing somewhere!");
+                return;
+            }
+
+            setQuantity(quantity + defaultWords.length);
+            defaultWords.map(item => {
+                setImportDefWords(oldElements => [...oldElements, item]);
+            })
+            translatedWords.map(item => {
+                setImportTransWords(oldElements => [...oldElements, item]);
+            })
+    }
+
+    function handleRemoveClick(e) {
+        if(quantity === 0) {
+            alert("There is nothing to remove!");
+            return;
+        }
+
+        let currentElement = Object.assign({}, e.currentTarget.parentElement.parentElement.dataset);
+        let key = Object.keys(currentElement)[0];
+        let index = Number(currentElement[key]);
+        
+        if(key === "iddefault") {
+            let newArray = [...wordFields];
+            newArray.splice(index, 1)
+            setWordFields(newArray);
+            setQuantity(q => q - 1);
+            return
+        }
+
+        if(key === "idimport") {
+            let newDefArray = [...importDefWords];
+            let newTransArray = [...importTransWords];
+            newDefArray.splice(index, 1);
+            newTransArray.splice(index, 1);
+            setQuantity(q => q - 1);
+            setImportDefWords(newDefArray);
+            setImportTransWords(newTransArray);
+        }
+    }
 
     function putToLocalStorage(card) {
         alert("Card has successfully added to collection!")
@@ -34,51 +141,36 @@ export default function NewCard() {
         setQuantity(quantity + 1);
     }
 
-    function handleRemoveWordButtonClick() {
-        if(quantity === 0) {
-            alert("There is no fields");
-            return;
-        }
-        if(wordFields.length > 0) {
-            let arr = wordFields
-            arr.pop();
-            setWordFields(arr);
-            setQuantity(quantity - 1);
-        }
-    }
-
-    function handleCardSubmit(event) {
-        event.preventDefault();
+    function handleCardSubmit(e) {
+        e.preventDefault();
 
         if(quantity === 0) {
             alert("You haven't added any words");
             return;
         }
 
-        const cardName = event.currentTarget.elements.cardName.value;
-        const cardDescription = event.currentTarget.elements.cardDescription.value;
+        const cardName = e.currentTarget.elements.cardName.value;
+        const cardDescription = e.currentTarget.elements.cardDescription.value;
 
         let t;
-        if(event.currentTarget.elements.cardFileUpload.files[0] === undefined) {
+        if(e.currentTarget.elements.cardFileUpload.files[0] === undefined) {
             t = "empty";
         } else {
-            t = event.currentTarget.elements.cardFileUpload.files[0];
+            t = e.currentTarget.elements.cardFileUpload.files[0];
         }
         const cardImage = t;
 
         const cardId = crypto.randomUUID();
 
-        console.log(event.currentTarget.elements.cardWordDefault);
-
         let cardWordDefaultArrayUnmapped = [];
         let cardWordTranslationArrayUnmapped = [];
 
         if(quantity === 1) {
-            cardWordDefaultArrayUnmapped.push(event.currentTarget.elements.cardWordDefault);
-            cardWordTranslationArrayUnmapped.push(event.currentTarget.elements.cardWordTranslation);
+            cardWordDefaultArrayUnmapped.push(e.currentTarget.elements.cardWordDefault);
+            cardWordTranslationArrayUnmapped.push(e.currentTarget.elements.cardWordTranslation);
         } else {
-            cardWordDefaultArrayUnmapped = Array.from(event.currentTarget.elements.cardWordDefault);
-            cardWordTranslationArrayUnmapped = Array.from(event.currentTarget.elements.cardWordTranslation);
+            cardWordDefaultArrayUnmapped = Array.from(e.currentTarget.elements.cardWordDefault);
+            cardWordTranslationArrayUnmapped = Array.from(e.currentTarget.elements.cardWordTranslation);
         }
 
         const cardWordDefaultArrayMapped = [];
@@ -88,7 +180,6 @@ export default function NewCard() {
 
         const cardWordTranslationArrayMapped = [];
         cardWordTranslationArrayUnmapped.map(item => {
-            console.log(item);
             cardWordTranslationArrayMapped.push(item.value);
         })
         
@@ -128,28 +219,37 @@ export default function NewCard() {
                 <input type="file" id="cardFileUpload" name="cardFileUpload" className="card-form-image-input"/>
 
                 <div className="card-add-button-word-container">
-                    <button className="card-button-add-word" type="button" onClick={() => handleAddWordButtonClick()}>Add a word</button>
-                    <button className="card-button-add-word" type="button" onClick={() => handleRemoveWordButtonClick()}>Remove a word</button>
+                    <button className="card-button-add-word" type="button" onClick={(e) => handleAddWordButtonClick(e)}>Add a word</button>
+                    <button className="card-button-add-word" type="button" onClick={(e) => handleImportClick(e)}>Import</button>
                 </div>
                 
 
                 {wordFields.map((item, key) => (
-                    <div className="card-form-word-container" key={key}>
-                        <div className="card-word-default-container">
-                            <label htmlFor="cardWordDefault" className="card-word-label">Enter a word: </label>
-                            <input type="text" id="cardWordDefault" name="cardWordDefault" className="card-word-input" placeholder="Enter a word" required/>
-                        </div>
-                        
-                        <div className="card-word-separator-container">
-                            <hr className="navigation-button-separator"/>
-                        </div>
+                    <div className="card-form-word-container-main" key={key} data-iddefault={key}>
+                        <div className="card-form-word-container">
+                            <div className="card-word-default-container">
+                                <label htmlFor="cardWordDefault" className="card-word-label">Enter a word: </label>
+                                <input type="text" id="cardWordDefault" name="cardWordDefault" className="card-word-input" placeholder="Enter a word" required/>
+                            </div>
+                            
+                            <div className="card-word-separator-container">
+                                <hr className="navigation-button-separator"/>
+                            </div>
 
-                        <div className="card-word-translation-container">
-                            <label htmlFor="cardWordTranslation" className="card-word-label">Enter a translation: </label>
-                            <input type="text" id="cardWordTranslation" name="cardWordTranslation" className="card-word-input" placeholder="Enter a translation" required/>
+                            <div className="card-word-translation-container">
+                                <label htmlFor="cardWordTranslation" className="card-word-label">Enter a translation: </label>
+                                <input type="text" id="cardWordTranslation" name="cardWordTranslation" className="card-word-input" placeholder="Enter a translation" required/>
+                            </div>
+                        </div>
+                        <div className="card-word-button-remove-container">
+                        <button className="card-word-button-remove" type="button" onClick={(e) => handleRemoveClick(e)}>
+                            <img src={green_icon} alt="Delete Icon" className="button-delete-image"/>
+                        </button>
                         </div>
                     </div>
                 ))}
+
+                {importedFields}
 
                 <hr className="navigation-button-separator"/>
                 <button type="submit" className="card-form-button-submit">Submit the Card</button>
--- src/main/App.jsx
+++ src/main/App.jsx
@@ -2,6 +2,7 @@ import AddNewCardPage from "../components/pages/AddNewCardPage";
 import MainPanel from "../components/pages/MainPanel";
 import AccountSettingsPage from "../components/pages/AccountSettingsPage"
 import LogInPage from "../components/pages/LogInPage"
+import ImportPage from "../components/pages/ImportPage"
 import DevPage from "../components/pages/DevPage"
 import CardPage from "../components/pages/CardPage";
 import NotFoundPage from "../components/pages/NotFoundPage";
@@ -20,6 +21,7 @@ export default function App() {
           <Route path="/addNewCardPage" element={<AddNewCardPage />}/>
           <Route path="/accountSettingsPage" element={<AccountSettingsPage />}/>
           <Route path="/logInPage" element={<DevPage />}/>
+          <Route path="/importPage" element={<ImportPage />}/>
           <Route path="/cardPage/:id" element={<CardPage />}/>
           <Route path="*" element={<NotFoundPage />}/>
         </Routes>
